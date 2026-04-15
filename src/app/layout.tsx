d75b9f89b1773fcefe4efb3df78e8a9cf0d6c966
negraodenio <negraodenio@gmail.com>
2026-04-15 16:58:17 +0100

    feat: update Plausible to custom account tracking script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,12 +24,16 @@ export default function RootLayout({
                     {children}
                     <Toaster />
                 </Providers>
+                {/* Privacy-friendly analytics by Plausible */}
                 <Script
-                    defer
-                    data-domain="roastthis.site"
-                    src="https://plausible.io/js/script.js"
+                    async
+                    src="https://plausible.io/js/pa-r2G6O2l14jajOQJHfV81J.js"
                     strategy="afterInteractive"
                 />
+                <Script id="plausible-init" strategy="afterInteractive">{`
+                    window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};
+                    plausible.init()
+                `}</Script>
             </body>
         </html>
     )
